4cb9870f6e3b54071ee324b9490c8d1e3a1ea0e1
Tuan Phan <tuanphan.fi@gmail.com>
2022-03-28 06:56:05 +0700

    Section1

---
+++ section1-introduction/test.js
@@ -0,0 +1,38 @@
+//  ECMAScript (ES)
+// ES5 vs ES6
+// ES5 supported by all browsers
+
+// Needs polyfills, transpilers
+// https://javascript.info/polyfills
+
+// ES6 Compatibility with Browsers
+// https://kangax.github.io/compat-table/es6/
+
+
+// Cheat Sheet – Local
+// Development Environment
+// What’s Required?
+// To develop ES6 applications locally, you need a transpiler (ES6 -> ES5), a
+// module loader (to load the different modules/ files) and a little server to
+// host your app (and to serve those module files).
+// Compiler Examples
+// Babel: https://babeljs.io/
+// Traceur: https://github.com/google/traceur-compiler
+// Module Loader / Packaging Examples
+// SystemJS: https://github.com/systemjs/systemjs
+// Webpack: https://github.com/webpack/webpack
+// Servers
+// Live-server: https://www.npmjs.com/package/live-server
+// Lite-server: https://github.com/johnpapa/lite-server
+// Webpack-dev-server: https://webpack.github.io/docs/webpack-devserver.html
+// A quick Way to get Started – JSPM
+// You could also use the JavaScript Package Manager (JSPM) to quickly set
+// up your own development environment with Compiler (Babel) and Module
+// Loader (SystemJS). You would need to install an extra server though
+// (google for jspm server for example).
+// Link: http://jspm.io/
+// Complete Setups / Boilerplates
+// The one from the video: https://github.com/topheman/vanilla-es6-jspm
+// Alternative: https://github.com/thoughtram/es6-babel-browserifyboilerplate
+// To find more, simply google for “ES6 Boilerplate” or comparable search
+// terms!
