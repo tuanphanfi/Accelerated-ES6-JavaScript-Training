40f2cec1a4d1bf9c0329c84cba841851314b6c63
Tuan Phan <tuanphan.fi@gmail.com>
2022-03-28 14:44:25 +0700

    inheritance

--- section3-modulesAndClasses/main.js
+++ section3-modulesAndClasses/main.js
@@ -4,12 +4,36 @@ class Person {
     }
 
     greet() {
-        console.log(`Hello ${this.name}`)
+        console.log(`Hello ${this.name} and I am ${this.age}`)
     }
 }
 
+
 let person = new Person('Max');
 
-person.greet()
+// person.greet()
 
+
+class Max extends Person {
+    constructor(age) {
+        super('Max'); //it calls parent constructor
+        this.age = age
+    }
+
+    greet() {
+        console.log('Hello')
+    }
+
+    greetTwice() {
+        super.greet()
+        super.greet()
+    }
+
+}
+
+let max = new Max(27)
 // inheritance
+
+// max.greet()
+max.greetTwice()
+// max.greet()
